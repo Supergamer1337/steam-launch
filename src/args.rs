64117690a10297488258ef1db5ca9c92335de9ed
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-01-28 11:08:16 +0100

    Launching extra programs

--- src/args.rs
+++ src/args.rs
@@ -6,35 +6,25 @@ use itertools::Itertools;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The command to launch the game with.
-    #[arg(short, long, num_args(2), value_names = ["Launch Option Name", "Launch Command"], required(true))]
-    launch: Vec<String>,
-
-    /// This is where you need to put %command% in the launch options.
-    _trailing_game_command: String,
+    /// Extra program to launch before the game.
+    #[arg(short = 'e', long = "extra")]
+    extra_programs: Vec<String>,
 }
 
 impl Args {
-    fn refine(self) -> RefinedArgs {
-        let launch = self
-            .launch
-            .into_iter()
-            .chunks(2)
-            .into_iter()
-            .map(|mut chunk| LaunchOption {
-                name: chunk.next().unwrap(),
-                command: chunk.next().unwrap(),
-            })
-            .collect();
-
+    fn refine(self, game_args: Vec<String>) -> RefinedArgs {
         RefinedArgs {
-            launch_options: launch,
+            extra_programs: self.extra_programs,
+            game_command: game_args[0].clone(),
+            game_args: game_args[1..].to_vec(),
         }
     }
 }
 
 pub struct RefinedArgs {
-    pub launch_options: Vec<LaunchOption>,
+    pub extra_programs: Vec<String>,
+    pub game_command: String,
+    pub game_args: Vec<String>,
 }
 
 #[derive(Clone, Debug)]
@@ -44,5 +34,12 @@ pub struct LaunchOption {
 }
 
 pub fn args() -> RefinedArgs {
-    Args::parse().refine()
+    let args = std::env::args().collect::<Vec<_>>();
+    let Some((args_to_parse, game_args)) = args.split_inclusive(|arg| arg == "--").collect_tuple()
+    else {
+        eprintln!("No game command specified.");
+        std::process::exit(1);
+    };
+
+    Args::parse_from(args_to_parse).refine(game_args.to_vec())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,19 @@
 mod args;
 
+use args::args;
 use std::process::Command;
-
-use args::{args, LaunchOption};
-use handlebars::Handlebars;
-use serde_json::json;
 
 fn main() {
     let args = args();
 
-    let picker = create_launch_picker(args.launch_options.iter().map(|x| &x.name).collect());
+    for program in args.extra_programs {
+        Command::new(&program)
+            .spawn()
+            .expect("Failed to launch extra program.");
+    }
 
-    let mut choice = None;
-    web_view::builder()
-        .title("Launch Options")
-        .content(web_view::Content::Html(picker))
-        .size(800, 600)
-        .resizable(true)
-        .debug(true)
-        .user_data(())
-        .invoke_handler(|webview, arg| {
-            if args
-                .launch_options
-                .iter()
-                .filter(|option| option.name == arg)
-                .count()
-                > 0
-            {
-                choice = Some(arg.to_string());
-                return Ok(webview.exit());
-            }
-
-            Ok(())
-        })
-        .run()
-        .unwrap();
-
-    let choice = choice.unwrap();
-    let option = args
-        .launch_options
-        .iter()
-        .find(|x| x.name == choice)
-        .unwrap()
-        .clone();
-
-    Command::new(option.command)
+    Command::new(args.game_command)
+        .args(args.game_args)
         .spawn()
         .expect("Failed to launch game.");
-}
-
-fn create_launch_picker(names: Vec<&String>) -> String {
-    let mut h = Handlebars::new();
-    let picker = include_str!("../markup/picker.html");
-    h.register_template_string("picker", picker).unwrap();
-    h.render("picker", &json!({"launchOptions": names}))
-        .unwrap()
 }
