951760ed2628e22d14b84e565bcdc491792dc042
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-01-28 16:34:33 +0100

    Now automatically kills the extra programs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![windows_subsystem = "windows"]
 mod args;
 
 use args::args;
@@ -6,14 +7,22 @@ use std::process::Command;
 fn main() {
     let args = args();
 
+    let mut extra_running = vec![];
     for program in args.extra_programs {
-        Command::new(&program)
-            .spawn()
-            .expect("Failed to launch extra program.");
+        extra_running.push(
+            Command::new(&program)
+                .spawn()
+                .expect("Failed to launch extra program."),
+        );
     }
 
-    Command::new(args.game_command)
+    let _ = Command::new(args.game_command)
         .args(args.game_args)
         .spawn()
-        .expect("Failed to launch game.");
+        .expect("Failed to launch game.")
+        .wait();
+
+    for mut process in extra_running {
+        let _ = process.kill();
+    }
 }
